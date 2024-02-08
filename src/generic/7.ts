08c196e6dc9d302620031df438420695ebffb1b4
RuslanKorni <ruslankornijcenko4@gmail.com>
2024-02-08 18:33:23 +0200

    finish commit

--- src/generic/7.ts
+++ src/generic/7.ts
@@ -4,16 +4,29 @@
 */
 
 export enum UserRole {
-  admin = 'admin',
-  editor = 'editor',
-  guest = 'guest',
+  admin = "admin",
+  editor = "editor",
+  guest = "guest",
 }
 
 // Замініть наступний код на версію за допомогою Record
-const RoleDescription = {
-  admin: 'Admin User',
-  editor: 'Editor User',
-  guest: 'Guest User',
+
+const RoleDescription: Record<UserRole, string> = {
+  [UserRole.admin]: "Admin User",
+  [UserRole.editor]: "Editor User",
+  [UserRole.guest]: "Guest User",
 };
+
+console.log(RoleDescription);
+
+//* 2 variant:
+// Для кращої читабельності винесли в окремий тип
+// type UserRoleStatus = Record<UserRole, string>;
+
+// const RoleDescription: UserRoleStatus = {
+//   [UserRole.admin]: 'Admin User',
+//   [UserRole.editor]: 'Editor User',
+//   [UserRole.guest]: 'Guest User',
+// };
 
 export {};
